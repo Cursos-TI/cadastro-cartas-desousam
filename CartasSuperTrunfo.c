b0eef0065d1ba50fca97d0abf4f6c536becef2c9
de Sousa <mariliacs0239@gmail.com>
2026-03-30 13:51:11 +0000

    adicionei as variáveis da carta1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,15 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  
+  // ============= CARTA 1 =============
+  char estado1;
+  char codigo1[4];
+  char cidade1[50];
+  int populacao1;
+  float area1;
+  float pib1;
+  int pontosTuristicos1;
 
   // Área para entrada de dados
 
